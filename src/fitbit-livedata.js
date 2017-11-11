3888df209407f7b885a49669e132530bcb35b8f4
horihiro <hori.kawa.shima.tool+github@gmail.com>
2017-11-12 06:00:00 +0900

    add status change

--- src/fitbit-livedata.js
+++ src/fitbit-livedata.js
@@ -4,30 +4,55 @@ export default (RED) => {
   const FitbitLivedataNode = function FitbitLivedataNode(config) {
     RED.nodes.createNode(this, config);
     const node = this;
+    const trackers = [];
+    const addresses = [];
+    let isRegisted = false;
+
+    node.status({ fill: 'red', shape: 'dot', text: 'scan stopped' });
+    fitbit.on('scanStop', () => {
+      if (trackers.filter(t => t.status === 'connected').length === 0) {
+        node.status({ fill: 'red', shape: 'dot', text: 'scan stopped' });
+      }
+    });
+    fitbit.on('scanStart', () => {
+      if (trackers.filter(t => t.status === 'connected').length === 0) {
+        node.status({ fill: 'blue', shape: 'ring', text: 'scanning...' });
+      }
+    });
     this.on('input', (msg) => {
-      const addresses = msg.payload.map(tracker => tracker.address.toLowerCase());
-      fitbit.on('discover', (tracker) => {
-        if (addresses.indexOf(tracker.peripheral.address.toLowerCase()) < 0) return;
-        tracker.on('disconnect', (data) => {
+      msg.payload.map(tracker => tracker.address.toLowerCase()).forEach((a) => {
+        if (addresses.indexOf(a) < 0) addresses.push(a);
+      });
+      if (!isRegisted) {
+        isRegisted = true;
+        fitbit.on('discover', (tracker) => {
+          trackers.push(tracker);
+          if (addresses.indexOf(tracker.peripheral.address.toLowerCase()) < 0) return;
+          tracker.on('disconnect', (data) => {
+            tracker.connect();
+          });
+        
+          tracker.on('connect', () => {
+            node.status({ fill: 'blue', shape: 'dot', text: 'connected' });
+            msg.payload = {
+              event: 'connected',
+            };
+            node.send(msg);
+          });
+          tracker.on('data', (livedata) => {
+            node.status({ fill: 'yellow', shape: 'dot', text: 'connected' });
+            setTimeout(() => {
+              node.status({ fill: 'blue', shape: 'dot', text: 'connected' });
+            }, 500);
+            msg.payload = {
+              event: 'data',
+              data: livedata,
+            };
+            node.send(msg);
+          });
           tracker.connect();
         });
-      
-        tracker.on('connect', () => {
-        });
-        tracker.on('openSession', (data) => {
-        });
-        tracker.on('authenticate', (data) => {
-        });
-        tracker.on('sendAuth', (data) => {
-        });
-        tracker.on('authenticated', (data) => {
-        });
-        tracker.on('data', (livedata) => {
-          msg.payload = livedata;
-          node.send(msg);
-        });
-        tracker.connect();
-      });
+      }
       fitbit.scanTrackers(msg.payload);
     });
   };
